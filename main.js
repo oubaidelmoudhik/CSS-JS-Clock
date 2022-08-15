b89f2c22b484d1ebbdabd82676d7c1d62d989fae
theniceguywastaken <99677066+theniceguywastaken@users.noreply.github.com>
2022-08-15 17:25:16 +0100

    First commit

---
+++ main.js
@@ -0,0 +1,20 @@
+const secondHand = document.querySelector(".second-hand");
+const minutesHand = document.querySelector(".min-hand");
+const hoursHand = document.querySelector(".hour-hand");
+
+function setDate() {
+  const now = new Date();
+  const seconds = now.getSeconds();
+  const minutes = now.getMinutes();
+  const hours = now.getHours();
+  const secondsDegree = (seconds / 60) * 360 + 90;
+  const minutesDegree = (minutes / 60) * 360 + 90;
+  const hoursDegree = (hours / 60) * 360 + 90;
+
+  secondHand.style.transform = `rotate(${secondsDegree}deg)`;
+  minutesHand.style.transform = `rotate(${minutesDegree}deg)`;
+  hoursHand.style.transform = `rotate(${hoursDegree}deg)`;
+  console.log(seconds);
+}
+
+setInterval(setDate, 1000);
